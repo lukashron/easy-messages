c333df93cc38ce52284bf8f378a5b4808c4bf231
Lukáš Hron <info@lukashron.cz>
2022-02-02 17:08:38 +0100

    MessagesTest -> testInit

---
+++ tests/MessagesTest.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is part of the Easy-messages package.
+ *
+ * (c) Lukas Hron <info@lukashron.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace LukasHron\EasyMessages\Tests;
+
+use LukasHron\EasyMessages\Messages;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/bootstrap.php';
+
+final class MessagesTest extends TestCase
+{
+    public function testInit()
+    {
+        $flashMessages = new Messages();
+        $this->assertFalse($flashMessages->init());
+
+        $sessionName = $flashMessages->getEmSessionName();
+        $this->assertTrue(strlen($sessionName) > 0);
+
+        unset($_SESSION[$sessionName]);
+        $this->assertTrue($flashMessages->init());
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the Easy-messages package.
+ *
+ * (c) Lukas Hron <info@lukashron.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../../vendor/autoload.php';
